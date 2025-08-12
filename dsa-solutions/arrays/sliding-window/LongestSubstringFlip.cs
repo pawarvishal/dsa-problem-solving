e500cd5ecf5c524db8a69ac40baa19bac0695daf
Vishal Pawar <openstack.vishal@gmail.com>
2025-08-12 06:32:36 +0530

    initial commit, added .gitingore to solution

---
+++ dsa-solutions/arrays/sliding-window/LongestSubstringFlip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsa_solutions.arrays.sliding_window
+{
+    /*
+    1. The Alien Transmission Decoder
+    You’re decoding an alien signal (a string of 0s and 1s). Find the longest segment with at most flips (0→1 or 1→0) to stabilize the transmission.
+    Problem: Given a binary string and k, return the length of the longest substring with ≤ k bit flips.
+    Examples:
+
+    "101101", k=1 → 5 (Flip the 3rd bit: "101111")
+
+    "00000", k=2 → 5 (No flips needed)
+
+    "110010", k=2 → 6 (Flip two 0s to 1s)
+
+*/
+
+    class LongestSubstringFlip
+    {
+
+    }
+}
---
+++ dsa-solutions/arrays/two-pointers/TwoSumRotatedSorted.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsa_solutions.arrays.two_pointers
+{
+    class TwoSumRotatedSorted
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 6, 7, 1, 2, 3, 4 };
+            int target = 9;
+            Console.Write(TwoSumRotatedSorted.TwoSum(nums, target));
+        }
+
+        private static bool TwoSum(int[] nums, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
